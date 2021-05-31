f7facb080e36304efe8c93bf7b2099411da1c75c
zeon77 <zeon77@gmail.com>
2021-05-31 09:31:51 +0200

    Add class.

---
+++ OpeningWeekend/Film.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OpeningWeekend
+{
+    class Film
+    {
+        public string EredetiCím { get; set; }
+        public string MagyarCím { get; set; }
+        public DateTime BemutatóDátum { get; set; }
+        public string Forgalmazó { get; set; }
+        public int Bevétel { get; set; }
+        public int LátógatókSzáma { get; set; }
+
+        public Film(string sor)
+        {
+            string[] splitted = sor.Split(';');
+            EredetiCím = splitted[0];
+            MagyarCím = splitted[1];
+            BemutatóDátum = DateTime.Parse(splitted[2]);
+            Forgalmazó = splitted[3];
+            Bevétel = int.Parse(splitted[4]);
+            LátógatókSzáma = int.Parse(splitted[5]);
+        }
+    }
+}
